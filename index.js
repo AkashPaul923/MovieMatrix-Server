afcff0e75929ba23079efba3deb4de263276db19
AkashPaul923 <akashpaul923@gmail.com>
2024-12-08 12:47:47 +0600

    update movie in database

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const movieCollection = client.db("movieDB").collection("movies")
     const favoriteCollection = client.db("movieDB").collection("favorites")
 
@@ -76,6 +76,28 @@ async function run() {
         res.send(result)
     })
 
+    app.put('/movies/:id',async (req, res)=>{
+        const id = req.params.id
+        const updatedMovie = req.body
+        const filter = {_id : new ObjectId(id)}
+        const options = { upsert: true }
+        const movie = {
+          $set: {
+            poster: updatedMovie.poster, 
+            title: updatedMovie.title, 
+            genre: updatedMovie.genre, 
+            year: updatedMovie.year, 
+            rating: updatedMovie.rating, 
+            duration: updatedMovie.duration, 
+            summery: updatedMovie.summery
+ 
+          }
+        }
+
+        const result = await movieCollection.updateOne(filter, movie, options)
+        res.send(result)
+    })
+
     app.delete("/movies/:id", async (req,res)=>{
       const id = req.params.id
       const query = {_id : new ObjectId(id)}
@@ -93,8 +115,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
